8e9ca734c58fd282025542ac5e61a5bd4ee7dc06
Alexandre Tessier <skaven27@gmail.com>
2025-03-28 13:52:05 +0000

    update nbom function

--- pkg/outils/outils.go
+++ pkg/outils/outils.go
@@ -3,7 +3,7 @@ package outils
 import "os"
 
 // getEnv get key environment variable if exist, otherwise return defaultValue
-func getEnv(key, defaultValue string) string {
+func GetEnv(key, defaultValue string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return defaultValue
